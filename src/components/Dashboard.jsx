0667fd135246441b1f85395000ca21963eca5b25
Jamil-4khtar <jamil4khtar@gmail.com>
2024-12-11 04:58:25 +0530

    add: name of user

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -5,7 +5,7 @@ import "../styles/Dashboard.css"
 
 
 const Dashboard = ({token ,setToken}) => {
-    const [pun, setPun] = useState("")
+    const [pun, setPun] = useState({})
     const [errorMsg, setErrorMsg] = useState("")
     const navigate = useNavigate()
     console.log("A")
@@ -30,8 +30,10 @@ const Dashboard = ({token ,setToken}) => {
                         Authorization: `Bearer ${token}`
                     }
                 })
-                console.log(response)
-                setPun(response.data.data.message)
+                // console.log(response.data)
+                let data = await response.data.data;
+                setPun(data)
+                console.log(pun)
                 setErrorMsg("")
             } catch (error) {
                 console.log(error)
@@ -61,10 +63,11 @@ const Dashboard = ({token ,setToken}) => {
   return (
     <div className='dashboard'>
         <button className='logout-button' onClick={handleLogout}>Log out</button>
+        {pun.user?.name && <h1 className='name'>Hey, {pun.user.name}! Checkout this joke from Zukerberg.</h1>}
     {pun && (
       <div className="comical">
         <div className="comical-content">
-          <q>{pun}</q>
+          <q>{pun.message}</q>
         </div>
       </div>
     )}
